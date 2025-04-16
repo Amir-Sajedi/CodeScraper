39b0267be910a56fe96b3bc8429a233222d09cba
Amir <manamir4179@gmail.com>
2025-04-16 15:09:54 +0330

    feat : add rabbit support

--- sql_script.py
+++ sql_script.py
@@ -1,42 +1,66 @@
+import pika
 import json
+import time
 import pymysql
 
-with open('E:/Sql_data/data.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
 
-db = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='12345678',  
-    database='scraped_data',
-    charset='utf8mb4'  
-)
+def rabbit_consume():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='scrapper_queue__MelkRadar')
+    while True:
+        # method_frame: metadata about the message (e.g., delivery tag, etc.) (is None if there aren't any messages received)
+        #
+        # header_frame: headers and properties (Not needed here)
+        #
+        # body: the actual message you sent from the detector
+        method_frame, header_frame, body = channel.basic_get(queue='scrapper_queue__MelkRadar', auto_ack=True)
+        if (method_frame):
+            data = json.loads(body)
+            database_publish(data)
+            time.sleep(10)
+        else:
+            time.sleep(10)
 
-cursor = db.cursor()
 
-for row in data:
-    sql = """
-    INSERT INTO properties (id, url, name, address, price, area, room_count, year, feats, images)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
-    
-    feats_json = json.dumps(row['feats'], ensure_ascii=False)
-    images_json = json.dumps(row['images'], ensure_ascii=False)
-    values = (
-        row['id'],
-        row['url'],
-        row['name'],
-        row['address'],
-        row['price'],
-        int(row['area']),
-        int(row['room_count']),
-        int(row['year']),
-        feats_json,
-        images_json
+def database_publish(data):
+    db = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='12345678',
+        database='codescrapper',
+        charset='utf8mb4'
     )
-    cursor.execute(sql, values)
+    cursor = db.cursor()
+    for listing in data:
+        listing = json.loads(listing)
+        sql = """
+         INSERT INTO listings (id, url, name, address, price, area, room_count, year, feats, images)
+         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+         """
+        feats_json = json.dumps(listing['feats'], ensure_ascii=False)
+        images_json = json.dumps(listing['images'], ensure_ascii=False)
+        values = (
+            listing['id'],
+            listing['url'],
+            listing['name'],
+            listing['address'],
+            f'{int(listing["price"]):,}' if str(listing['price']).isnumeric() else listing['price'],
+            listing['area'],
+            listing['room_count'],
+            int(listing['year']) if listing['year'] else None,
+            feats_json,
+            images_json
+        )
+        try:
+            cursor.execute(sql, values)
+        except Exception as e:
+            print("Error", e)
+    db.commit()
+    cursor.close()
+    db.close()
+    print("Data got transfered!")
 
-db.commit()
-cursor.close()
-db.close()
-print("Data got transfered!")
+
+if __name__ == '__main__':
+    rabbit_consume()
